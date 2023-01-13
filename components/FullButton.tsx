681303278c7b1c8da21fbc98700127114b3c0fce
hone1er <24376928+hone1er@users.noreply.github.com>
2023-01-13 10:08:53 -0800

    refactor fullbutton

--- components/FullButton.tsx
+++ components/FullButton.tsx
@@ -1,58 +1,49 @@
-import React from 'react'
-import {
-	Center,
-	Text,
-	VStack,
-	Square,
-	GridItem,
-	LinkBox,
-	LinkOverlay,
-	Heading,
-} from '@chakra-ui/react'
-import { FaCat, FaDog } from 'react-icons/fa'
-import { GiHouse } from 'react-icons/gi'
-import { RiPlantFill } from 'react-icons/ri'
+import React from "react";
+import { Heading, Flex, ChakraProps } from "@chakra-ui/react";
+import { FaCat, FaDog } from "react-icons/fa";
+import { GiHouse } from "react-icons/gi";
+import { RiPlantFill } from "react-icons/ri";
+import Link from "next/link"; // use the link from next.js for its prefetching https://nextjs.org/docs/api-reference/next/link
 
-type FullButtonProps = {
-	bgColor: string
-	topText?: string
-	bottomText?: string
-	icon?: string
-	href?: string
+// extend the props from chakra-ui to have access to all the props from chakra-ui
+interface FullButtonProps extends ChakraProps {
+  topText?: string;
+  bottomText?: string;
+  icon?: string;
+  href?: string;
 }
+// FullButton is a React functional component(React.FC) that takes in FullButtonProps and returns a JSX.Element
+// React.FC gives us access to children so we can pass in children to the component without having to define them in the props
+const FullButton: React.FC<FullButtonProps> = (
+  props,
+  children
+): JSX.Element => {
+  // destructure the known props and spread the rest of the props to the Flex component
+  const { topText, bottomText, icon, href, ...rest } = props;
+  return (
+    <Link href={href || "#"}>
+      <Flex
+        justifyContent={"center"}
+        alignItems={"center"}
+        width={"100%"}
+        flexDir={"column"}
+        {...rest}
+      >
+        {topText && <Heading>{topText}</Heading>}
+        {icon
+          ? {
+              house: <GiHouse className="fullButtonIcon" />,
+              home: <GiHouse className="fullButtonIcon" />,
+              plant: <RiPlantFill className="fullButtonIcon" />,
+              cat: <FaCat className="fullButtonIcon" />,
+              dog: <FaDog className="fullButtonIcon" />,
+            }[icon]
+          : undefined}
+        {children}
+        {bottomText && <Heading textAlign={"center"}>{bottomText}</Heading>}
+      </Flex>
+    </Link>
+  );
+};
 
-const FullButton = ({
-	bgColor,
-	topText,
-	bottomText,
-	icon,
-	href,
-}: FullButtonProps) => {
-	return (
-		<LinkBox className='fullButton'>
-			<GridItem bgColor={bgColor}>
-				<Square>
-					<Center h={'45vh'}>
-						<VStack maxH={'99%'} p={3}>
-							<Heading>{topText}</Heading>
-							<div>
-								{icon &&
-									{
-										house: <GiHouse className='fullButtonIcon' />,
-										home: <GiHouse className='fullButtonIcon' />,
-										plant: <RiPlantFill className='fullButtonIcon' />,
-										cat: <FaCat className='fullButtonIcon' />,
-										dog: <FaDog className='fullButtonIcon' />,
-									}[icon]}
-							</div>
-							<Heading textAlign={'center'}>{bottomText}</Heading>
-							<LinkOverlay href={href} />
-						</VStack>
-					</Center>
-				</Square>
-			</GridItem>
-		</LinkBox>
-	)
-}
-
-export default FullButton
+export default FullButton;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,42 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import FullButton from '../components/FullButton'
-import { Box, Stack, Wrap, Grid } from '@chakra-ui/react'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import FullButton from "../components/FullButton";
+import { Box, Stack, Wrap, Grid, Text } from "@chakra-ui/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-	return (
-		<>
-			<Box>
-				<Grid templateColumns='repeat(2, 1fr)'>
-					<FullButton
-						bgColor={'green.300'}
-						// topText='plant'
-						// bottomText='BotHome'
-						icon='plant'
-						href='#'
-					/>
-					<FullButton
-						bgColor={'orange.300'}
-						topText='Dog House'
-						bottomText='Enter if you dare'
-						icon='dog'
-						href='#'
-					/>
-				</Grid>
-				<FullButton
-					bgColor={'blue.300'}
-					bottomText='Home'
-					icon='house'
-					href='#'
-				/>
-			</Box>
-		</>
-	)
+  return (
+    <>
+      <Box>
+        <Grid templateColumns="repeat(2, 1fr)">
+          <FullButton
+            bgColor={"green.300"}
+            height="50vh"
+            // topText='plant'
+            // bottomText='BotHome'
+            icon="plant"
+            href="/plants"
+          />
+          <FullButton
+            bgColor={"orange.300"}
+            height="50vh"
+            topText="Dog House"
+            bottomText="Enter if you dare"
+            icon="dog"
+            href="/pets"
+          />
+        </Grid>
+        <FullButton
+          width="100vw"
+          bgColor={"blue.300"}
+          height="50vh"
+          bottomText="Home"
+          icon="house"
+          href="/house"
+        />
+      </Box>
+    </>
+  );
 }
