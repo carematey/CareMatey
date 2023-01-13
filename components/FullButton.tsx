c1b961cec5f7860610a6cbbbb9dffa8ec761e668
hone1er <24376928+hone1er@users.noreply.github.com>
2023-01-13 10:20:25 -0800

    update prettier to use single quotes

--- components/FullButton.tsx
+++ components/FullButton.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { Heading, Flex, ChakraProps } from "@chakra-ui/react";
-import { FaCat, FaDog } from "react-icons/fa";
-import { GiHouse } from "react-icons/gi";
-import { RiPlantFill } from "react-icons/ri";
-import Link from "next/link"; // use the link from next.js for its prefetching https://nextjs.org/docs/api-reference/next/link
+import React from 'react';
+import { Heading, Flex, ChakraProps } from '@chakra-ui/react';
+import { FaCat, FaDog } from 'react-icons/fa';
+import { GiHouse } from 'react-icons/gi';
+import { RiPlantFill } from 'react-icons/ri';
+import Link from 'next/link'; // use the link from next.js for its prefetching https://nextjs.org/docs/api-reference/next/link
 
 // extend the props from chakra-ui to have access to all the props from chakra-ui
 interface FullButtonProps extends ChakraProps {
@@ -21,26 +21,26 @@ const FullButton: React.FC<FullButtonProps> = (
   // destructure the known props and spread the rest of the props to the Flex component
   const { topText, bottomText, icon, href, ...rest } = props;
   return (
-    <Link href={href || "#"}>
+    <Link href={href || '#'}>
       <Flex
-        justifyContent={"center"}
-        alignItems={"center"}
-        width={"100%"}
-        flexDir={"column"}
+        justifyContent={'center'}
+        alignItems={'center'}
+        width={'100%'}
+        flexDir={'column'}
         {...rest}
       >
         {topText && <Heading>{topText}</Heading>}
         {icon
           ? {
-              house: <GiHouse className="fullButtonIcon" />,
-              home: <GiHouse className="fullButtonIcon" />,
-              plant: <RiPlantFill className="fullButtonIcon" />,
-              cat: <FaCat className="fullButtonIcon" />,
-              dog: <FaDog className="fullButtonIcon" />,
+              house: <GiHouse className='fullButtonIcon' />,
+              home: <GiHouse className='fullButtonIcon' />,
+              plant: <RiPlantFill className='fullButtonIcon' />,
+              cat: <FaCat className='fullButtonIcon' />,
+              dog: <FaDog className='fullButtonIcon' />,
             }[icon]
           : undefined}
         {children}
-        {bottomText && <Heading textAlign={"center"}>{bottomText}</Heading>}
+        {bottomText && <Heading textAlign={'center'}>{bottomText}</Heading>}
       </Flex>
     </Link>
   );
--- pages/plants.tsx
+++ pages/plants.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const Plants = () => {
   return <div>Plants</div>;
