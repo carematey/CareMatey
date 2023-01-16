f8e3fc8fedb42ef0858b89d14b8e72f50827cf54
hone1er <24376928+hone1er@users.noreply.github.com>
2023-01-16 10:53:40 -0800

    add prisma

--- pages/pets.tsx
+++ pages/pets.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Pets = () => {
-  return <div>Pets</div>;
+    useEffect(() => {
+        fetch(`/api/pets/123`);
+
+        return () => {};
+    }, []);
+
+    return <div>Pets</div>;
 };
 
 export default Pets;
